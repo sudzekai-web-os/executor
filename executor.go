b29411a6f844a4a4db965988dd4bd87b0aae4216
sudzekai <sudzekai@hotmail.com>
2026-09-13 17:26:11 +0300

    трансфер пакета

---
+++ executor.go
@@ -0,0 +1,55 @@
+package executor
+
+import (
+	"bytes"
+	"os/exec"
+	"strings"
+
+	"github.com/sudzekai/web-os-api/packages/abstractions"
+	"github.com/sudzekai/web-os-api/packages/types"
+)
+
+type Executor struct {
+	logger abstractions.ILogger
+}
+
+func NewExecutor(factory abstractions.ILoggerFactory) abstractions.IExecutor {
+	return &Executor{
+		logger: factory.NewLogger("executor"),
+	}
+}
+
+func (ex *Executor) Execute(command string, args ...string) types.CommandResult {
+	cmd := exec.Command(command, args...)
+
+	return ex.executeCommand(cmd)
+}
+
+func (ex *Executor) ExecuteInDirectory(directory string, command string, args ...string) types.CommandResult {
+	cmd := exec.Command(command, args...)
+
+	cmd.Dir = directory
+
+	return ex.executeCommand(cmd)
+}
+
+func (ex *Executor) executeCommand(cmd *exec.Cmd) (result types.CommandResult) {
+	var stdoutBuf bytes.Buffer
+	var stderrBuf bytes.Buffer
+
+	cmd.Stdout = &stdoutBuf
+	cmd.Stderr = &stderrBuf
+
+	ex.logger.LogDebug("выполнение команды: %s %s", cmd.Path, strings.Join(cmd.Args, " "))
+
+	if result.Error = cmd.Start(); result.Error != nil {
+		return
+	}
+
+	result.Error = cmd.Wait()
+
+	result.Stdout = stdoutBuf.String()
+	result.Stderr = stderrBuf.String()
+
+	return
+}
